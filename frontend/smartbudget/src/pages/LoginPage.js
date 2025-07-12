f49008b58c1e18b584ecae559ec7d59ddbc1cfba
Joshua Kassie <kassiejoshua57@gmail.com>
2025-07-12 23:42:11 +0100

    testing waters with rough login page

---
+++ frontend/smartbudget/src/pages/LoginPage.js
@@ -0,0 +1,18 @@
+function LoginPage() {
+  return (
+    <div className="login-page">
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
