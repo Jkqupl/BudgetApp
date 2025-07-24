ebe8fd11b6f222807195ad2eb1e6bb00bf4af1ce
Joshua Kassie <kassiejoshua57@gmail.com>
2025-07-24 14:54:42 +0100

    tailwind is now working

--- frontend/smartbudget/src/App.js
+++ frontend/smartbudget/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
+import './index.css';
 import './App.css';
-import './global.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element = {<LoginPage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element = {<LoginPage/>} />
         {/* <Route path ="/MainPage" element ={<MainPage/>}/> */}
         {/* <Route path = "/RankingsPage" element = {<RankingsPage/>}/> */}
         {/* <Route path = "/Leaderboard" element = {<Leaderboard/>}/> */}
---
+++ frontend/smartbudget/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Smart Budget</h1>
+      <p>Your personal finance management tool.</p>
+    </div>
+  );
+}
+export default HomePage;
