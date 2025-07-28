f0a1c167ea3f0881c7d21b771b92b6c4a258e0e1
Joshua Kassie <kassiejoshua57@gmail.com>
2025-07-28 15:18:45 +0100

    deleting temp text

--- frontend/smartbudget/src/pages/DashboardPage.js
+++ frontend/smartbudget/src/pages/DashboardPage.js
@@ -3,7 +3,7 @@ function DashboardPage() {
     <div>
       <h1>Welcome to Smart Budget</h1>
       <p>This is the main page holding the dashboard</p>
-      <p>Your personal finance management tool. HHHHHHHH</p>
+      <p>Your personal finance management tool.</p>
     </div>
   );
 }
